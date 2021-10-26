b8e6d3737f87ca9f0c1d6a9163d5f4ad29417ded
Anjan Roy <anjanroy@yandex.com>
2021-10-26 10:49:13 +0000

    removed namespace using statement

--- ff.cpp
+++ ff.cpp
@@ -1,8 +1,6 @@
 #include "ff.hpp"
 #include <stdexcept>
 
-using namespace sycl;
-
 uint32_t ff_add(const uint32_t a, const uint32_t b) { return a ^ b; }
 
 uint32_t ff_sub(const uint32_t a, const uint32_t b) { return a ^ b; }
