65a879c13257697647d3e29f778bce80b9f5f3ca
Anjan Roy <anjanroy@yandex.com>
2021-10-26 10:19:51 +0000

    field multiplication implementation

--- ff.cpp
+++ ff.cpp
@@ -5,3 +5,31 @@ using namespace sycl;
 uint32_t ff_add(const uint32_t a, const uint32_t b) { return a ^ b; }
 
 uint32_t ff_sub(const uint32_t a, const uint32_t b) { return a ^ b; }
+
+uint32_t ff_neg(const uint32_t a) { return a; }
+
+uint32_t ff_mult(const uint32_t a, const uint32_t b) {
+  uint32_t a_ = a;
+  uint32_t b_ = b;
+  uint32_t c = 0;
+
+  if (b > a) {
+    a_ = b;
+    b_ = a;
+  }
+
+  while (b_ > 0) {
+    if (b_ & 0b1) {
+      c ^= a_;
+    }
+
+    b_ >>= 1;
+    a_ <<= 1;
+
+    if(a_ >= ORDER) {
+      a_ ^= IRREDUCIBLE_POLY;
+    }
+  }
+
+  return c;
+}
--- include/ff.hpp
+++ include/ff.hpp
@@ -1,8 +1,25 @@
 #include <CL/sycl.hpp>
 
+inline constexpr uint32_t CHARACTERISTIC = 0b10;
+inline constexpr uint32_t DEGREE = 0b100000;
+inline constexpr uint32_t ORDER = CHARACTERISTIC << (DEGREE - 0b1);
+// irreducible polynomial for 2**32 field:
+// x^32 + x^15 + x^9 + x^7 + x^4 + x^3 + 1
+inline constexpr uint32_t IRREDUCIBLE_POLY =
+    0b100000000000000001000001010011001;
+
 // adds two finite field elements
 uint32_t ff_add(const uint32_t a, const uint32_t b);
 
 // subtracts one field element from another one, implementation
 // is same as `ff_add`
 uint32_t ff_sub(const uint32_t a, const uint32_t b);
+
+// negation of one finite field element is same
+// as the given number, because each field element
+// is additive inverse to self
+uint32_t ff_neg(const uint32_t a);
+
+// multiplies two finite field elements
+// while respecting field rules
+uint32_t ff_mult(const uint32_t a, const uint32_t b);
