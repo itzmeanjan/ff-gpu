79af86aa3b77ff0f9794fa340d9902a7e557c5db
Anjan Roy <anjanroy@yandex.com>
2021-10-27 03:23:27 +0000

    declared kernel callable functions with `SYCL_EXTERNAL` attribute

--- include/ff.hpp
+++ include/ff.hpp
@@ -1,4 +1,5 @@
-#include <cstdint>
+#pragma once
+#include <CL/sycl.hpp>
 
 // extension field of which prime number
 inline constexpr uint32_t CHARACTERISTIC = 0b10;
@@ -12,30 +13,30 @@ inline constexpr uint64_t IRREDUCIBLE_POLY =
     0b100000000000000001000001010011001;
 
 // adds two finite field elements
-uint32_t ff_add(const uint32_t a, const uint32_t b);
+extern SYCL_EXTERNAL uint32_t ff_add(const uint32_t a, const uint32_t b);
 
 // subtracts one field element from another one, implementation
 // is same as `ff_add`
-uint32_t ff_sub(const uint32_t a, const uint32_t b);
+extern SYCL_EXTERNAL uint32_t ff_sub(const uint32_t a, const uint32_t b);
 
 // negation of one finite field element is same
 // as the given number, because each field element
 // is additive inverse to self
-uint32_t ff_neg(const uint32_t a);
+extern SYCL_EXTERNAL uint32_t ff_neg(const uint32_t a);
 
 // multiplies two finite field elements
 // while respecting field rules
-uint32_t ff_mult(const uint32_t a, const uint32_t b);
+extern SYCL_EXTERNAL uint32_t ff_mult(const uint32_t a, const uint32_t b);
 
 // inverts a field element i.e. finds multiplicative
 // inverse of it, given that it's not
 // additive identity of field
-uint32_t ff_inv(const uint32_t a);
+extern SYCL_EXTERNAL uint32_t ff_inv(const uint32_t a);
 
 // divides one field element by another one, which is nothing
 // but multiplying numerator with multiplicative inverse of
 // denominator
-uint32_t ff_div(const uint32_t a, const uint32_t b);
+extern SYCL_EXTERNAL uint32_t ff_div(const uint32_t a, const uint32_t b);
 
 // raises field element to power b, given b <= 0 || b > 0
-uint32_t ff_pow(const uint32_t a, const int32_t b);
+extern SYCL_EXTERNAL uint32_t ff_pow(const uint32_t a, const int32_t b);
