d17d78108223ce3a7959d87d1544027b15cc40bb
Anjan Roy <anjanroy@yandex.com>
2021-10-27 03:54:01 +0000

    hilbert matrix with field elements generation test on 1024 x 1024 matrix

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "hilbert.hpp"
-#include "utils.hpp"
+#include <chrono>
 
 using namespace sycl;
 
-const uint32_t N = 4;
-const uint32_t B = 4;
+const uint32_t N = 1 << 10;
+const uint32_t B = 1 << 5;
 
 int main(int argc, char **argv) {
   device d{default_selector{}};
@@ -13,8 +13,18 @@ int main(int argc, char **argv) {
             << std::endl;
 
   uint32_t *mat = (uint32_t *)malloc(sizeof(uint32_t) * N * N);
+
+  std::chrono::_V2::steady_clock::time_point start =
+      std::chrono::steady_clock::now();
   gen_hilbert_matrix(q, mat, N, B);
-  show_matrix(mat, N);
+  std::chrono::_V2::steady_clock::time_point end =
+      std::chrono::steady_clock::now();
+
+  int64_t tm =
+      std::chrono::duration_cast<std::chrono::milliseconds>(end - start)
+          .count();
+  std::cout << N << "x" << N << " hilbert matrix with F(2 ** 32) elements, in "
+            << tm << " ms" << std::endl;
 
   std::free(mat);
 
