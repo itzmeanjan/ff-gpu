b868b8e9ef53988436eb8f7cfb7779ed55257acb
Anjan Roy <anjanroy@yandex.com>
2021-10-27 02:58:27 +0000

    main function to run some testing

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "hilbert.hpp"
+#include "utils.hpp"
+
+using namespace sycl;
+
+const uint32_t N = 4;
+const uint32_t B = 4;
+
+int main(int argc, char *argv) {
+  device d{default_selector{}};
+  queue q{d};
+  std::cout << "running on " << d.get_info<info::device::name>() << "\n"
+            << std::endl;
+
+  uint32_t *mat = (uint32_t *)malloc(sizeof(uint32_t) * N * N);
+  gen_hilbert_matrix(q, mat, N, B);
+  show_matrix(mat, N);
+
+  std::free(mat);
+
+  return 0;
+}
