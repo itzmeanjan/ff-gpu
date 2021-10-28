fc08d2212528317d395620092fae2cd4c9fb07e8
Anjan Roy <anjanroy@yandex.com>
2021-10-28 14:07:27 +0000

    prime field modular multiplication op

--- ff_p.cpp
+++ ff_p.cpp
@@ -38,3 +38,32 @@ uint64_t ff_p_sub(uint64_t a, uint64_t b) {
 
   return res;
 }
+
+uint64_t ff_p_mul(uint64_t a, uint64_t b) {
+  a %= MOD;
+  b %= MOD;
+
+  uint128_t c_u128 = sycl::ulong2(a * b, sycl::mul_hi(a, b));
+
+  uint64_t ab = c_u128.x();
+  uint64_t cd = c_u128.y();
+  uint64_t c = cd & 0x00000000ffffffff;
+  uint64_t d = cd >> 32;
+
+  uint64_t res_0 = ab - d;
+  bool under_0 = ab < d;
+
+  uint32_t zero = 0;
+  uint64_t tmp_0 = (uint64_t)(zero - (uint32_t)(under_0 ? 1 : 0));
+  res_0 -= tmp_0;
+
+  uint64_t tmp_1 = (c << 32) - c;
+
+  uint64_t res_1 = res_0 + tmp_1;
+  bool over_0 = res_0 > UINT64_MAX - tmp_1;
+
+  uint64_t tmp_2 = (uint64_t)(zero - (uint32_t)(over_0 ? 1 : 0));
+  uint64_t res = res_1 + tmp_2;
+
+  return res;
+}
--- include/ff_p.hpp
+++ include/ff_p.hpp
@@ -1,6 +1,11 @@
 #pragma once
 #include <CL/sycl.hpp>
 
+// custom data type for dealing with two
+// 64-bit field element multiplication
+// resulting into 128-bit integer
+typedef sycl::cl_ulong2 uint128_t;
+
 // Prime modulas of selected field, F_p,
 // where p = 2 ** 64 - 2 ** 32 + 1
 inline constexpr uint64_t MOD =
@@ -17,3 +22,13 @@ uint64_t ff_p_add(uint64_t a, uint64_t b);
 // note: if a, b ∉ F_p, it will be converted by performing
 // {a, b} % MOD
 uint64_t ff_p_sub(uint64_t a, uint64_t b);
+
+// modular mulitiplication of two prime field elements
+//
+// note: if a, b ∉ F_p, it will be converted by performing
+// {a, b} % MOD
+//
+// multiplication results into 128-bit integer, which
+// is reduced to field element such that c ∈ [0, p)
+// where p = field prime modulas
+extern SYCL_EXTERNAL uint64_t ff_p_mul(uint64_t a, uint64_t b);
