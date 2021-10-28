63e5387882fd1f84c6c7b69064d949529b8e6ad0
Anjan Roy <anjanroy@yandex.com>
2021-10-28 10:54:55 +0000

    prime field addition

---
+++ ff_p.cpp
@@ -0,0 +1,18 @@
+#include "ff_p.hpp"
+#include <climits>
+
+uint64_t ff_p_add(const uint64_t a, const uint64_t b) {
+  uint64_t res_0 = a + b;
+  bool over_0 = a > UINT64_MAX - b;
+
+  uint32_t zero = 0;
+  uint64_t tmp_0 = (uint64_t)(zero - (uint32_t)(over_0 ? 1 : 0));
+
+  uint64_t res_1 = res_0 + tmp_0;
+  bool over_1 = res_0 > UINT64_MAX - tmp_0;
+
+  uint64_t tmp_1 = (uint64_t)(zero - (uint32_t)(over_1 ? 1 : 0));
+
+  uint64_t res = res_1 + tmp_1;
+  return res;
+}
