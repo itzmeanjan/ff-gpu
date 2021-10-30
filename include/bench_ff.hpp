1a2f316a7df76c7e3423603910d60857e9aa1734
Anjan Roy <anjanroy@yandex.com>
2021-10-30 03:06:12 +0000

    benchmark inversion on binary field

--- include/bench_ff.hpp
+++ include/bench_ff.hpp
@@ -17,3 +17,8 @@ void benchmark_ff_multiplication(sycl::queue &q, const uint32_t dim,
 // Benchmark binary extension field division performance
 void benchmark_ff_division(sycl::queue &q, const uint32_t dim,
                            const uint32_t wg_size, const uint32_t itr_count);
+
+// Benchmark binary extension field inversion ( multiplicative inverse op )
+// performance
+void benchmark_ff_inversion(sycl::queue &q, const uint32_t dim,
+                            const uint32_t wg_size, const uint32_t itr_count);
