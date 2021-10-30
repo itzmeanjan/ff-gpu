ad8ef4ab101778d5bcd0bcf097f9e7eec37db253
Anjan Roy <anjanroy@yandex.com>
2021-10-30 04:50:11 +0000

    benchmark prime field subtraction

--- include/bench_ff_p.hpp
+++ include/bench_ff_p.hpp
@@ -4,3 +4,8 @@
 // Benchmark addition on prime field F(2**64 - 2**32 + 1) elements
 void benchmark_ff_p_addition(sycl::queue &q, const uint32_t dim,
                              const uint32_t wg_size, const uint32_t itr_count);
+
+// Benchmark subtraction on prime field F(2**64 - 2**32 + 1) elements
+void benchmark_ff_p_subtraction(sycl::queue &q, const uint32_t dim,
+                                const uint32_t wg_size,
+                                const uint32_t itr_count);
