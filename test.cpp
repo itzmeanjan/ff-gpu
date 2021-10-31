0a2231a4ec5012fc3775e197b58a8c69b289dafa
Anjan Roy <anjanroy@yandex.com>
2021-10-31 06:13:17 +0000

    generic function to ease testing of field arithmetics

---
+++ test.cpp
@@ -0,0 +1,48 @@
+#include "ff_p.hpp"
+
+enum Op { add, sub, mult, pow, inv, div };
+
+// generic function to operate on field element operands,
+// specified using operator enumeration; computation offloaded to device
+//
+// returned result will ∈ F_p
+uint64_t operate(sycl::queue &q, uint64_t operand_1, uint64_t operand_2,
+                 Op op) {
+  uint64_t res = 0;
+
+  {
+    sycl::buffer<uint64_t, 1> buf{&res, sycl::range<1>{1}};
+
+    q.submit([&](sycl::handler &h) {
+      sycl::accessor<uint64_t, 1, sycl::access::mode::write,
+                     sycl::access::target::global_buffer>
+          acc{buf, h};
+
+      h.single_task([=]() {
+        switch (op) {
+        case add:
+          acc[0] = ff_p_add(operand_1, operand_2);
+          break;
+        case sub:
+          acc[0] = ff_p_sub(operand_1, operand_2);
+          break;
+        case mult:
+          acc[0] = ff_p_mult(operand_1, operand_2);
+          break;
+        case pow:
+          acc[0] = ff_p_pow(operand_1, operand_2);
+          break;
+        case inv:
+          acc[0] = ff_p_inv(operand_1);
+          break;
+        case div:
+          acc[0] = ff_p_div(operand_1, operand_2);
+          break;
+        }
+      });
+    });
+    q.wait();
+  }
+
+  return res % MOD;
+}
