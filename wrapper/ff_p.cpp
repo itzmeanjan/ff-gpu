5070288daa09ec0a5a7b5737730ff5d1d73019b6
Anjan Roy <anjanroy@yandex.com>
2021-11-02 14:05:06 +0000

    added c wrapper functions for all prime field arithmetics

--- wrapper/ff_p.cpp
+++ wrapper/ff_p.cpp
@@ -12,3 +12,68 @@ extern "C" uint64_t add(uint64_t a, uint64_t b) {
 
   return *res;
 }
+
+extern "C" uint64_t sub(uint64_t a, uint64_t b) {
+  sycl::device d{sycl::default_selector{}};
+  sycl::queue q{d};
+
+  uint64_t *res = (uint64_t *)sycl::malloc_shared(sizeof(uint64_t), q);
+
+  q.memset(res, 0, sizeof(uint64_t));
+  q.single_task([=]() { *res = ff_p_sub(a, b) % MOD; });
+  q.wait();
+
+  return *res;
+}
+
+extern "C" uint64_t mult(uint64_t a, uint64_t b) {
+  sycl::device d{sycl::default_selector{}};
+  sycl::queue q{d};
+
+  uint64_t *res = (uint64_t *)sycl::malloc_shared(sizeof(uint64_t), q);
+
+  q.memset(res, 0, sizeof(uint64_t));
+  q.single_task([=]() { *res = ff_p_mult(a, b) % MOD; });
+  q.wait();
+
+  return *res;
+}
+
+extern "C" uint64_t pow(uint64_t a, uint64_t b) {
+  sycl::device d{sycl::default_selector{}};
+  sycl::queue q{d};
+
+  uint64_t *res = (uint64_t *)sycl::malloc_shared(sizeof(uint64_t), q);
+
+  q.memset(res, 0, sizeof(uint64_t));
+  q.single_task([=]() { *res = ff_p_pow(a, b) % MOD; });
+  q.wait();
+
+  return *res;
+}
+
+extern "C" uint64_t inv(uint64_t a) {
+  sycl::device d{sycl::default_selector{}};
+  sycl::queue q{d};
+
+  uint64_t *res = (uint64_t *)sycl::malloc_shared(sizeof(uint64_t), q);
+
+  q.memset(res, 0, sizeof(uint64_t));
+  q.single_task([=]() { *res = ff_p_inv(a) % MOD; });
+  q.wait();
+
+  return *res;
+}
+
+extern "C" uint64_t div(uint64_t a, uint64_t b) {
+  sycl::device d{sycl::default_selector{}};
+  sycl::queue q{d};
+
+  uint64_t *res = (uint64_t *)sycl::malloc_shared(sizeof(uint64_t), q);
+
+  q.memset(res, 0, sizeof(uint64_t));
+  q.single_task([=]() { *res = ff_p_div(a, b) % MOD; });
+  q.wait();
+
+  return *res;
+}
