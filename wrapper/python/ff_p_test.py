b14da7d98ec6d515ba93908c0b8d0b73d0125927
Anjan Roy <anjanroy@yandex.com>
2021-11-03 15:54:42 +0000

    generate random field elements from [0, 2^64 - 1]

--- wrapper/python/ff_p_test.py
+++ wrapper/python/ff_p_test.py
@@ -3,46 +3,50 @@
 import galois as gl
 from ff_p import *
 from time import time
+from random import randint, seed
 
+MIN = 0
+MAX = 2 ** 64 - 1
 MOD = 2 ** 64 - 2**32 + 1
 ROUNDS = 1 << 7
 
 
 def main():
+    seed(time())
     gf = gl.GF(MOD)
 
     start = time()
     for _ in range(ROUNDS):
-        a = gf.Random()
-        b = gf.Random()
-        assert (a + b) == ff_p_add(a.item(), b.item())
+        a = randint(MIN, MAX)
+        b = randint(MIN, MAX)
+        assert (gf(a % MOD) + gf(b % MOD)) == ff_p_add(a, b)
 
     print(
         f'passed {ROUNDS} randomized {"addition":>16} tests !\t{time() - start:>16.2f} s')
 
     start = time()
     for _ in range(ROUNDS):
-        a = gf.Random()
-        b = gf.Random()
-        assert (a - b) == ff_p_sub(a.item(), b.item())
+        a = randint(MIN, MAX)
+        b = randint(MIN, MAX)
+        assert (gf(a % MOD) - gf(b % MOD)) == ff_p_sub(a, b)
 
     print(
         f'passed {ROUNDS} randomized {"subtraction":>16} tests !\t{time() - start:>16.2f} s')
 
     start = time()
     for _ in range(ROUNDS):
-        a = gf.Random()
-        b = gf.Random()
-        assert (a * b) == ff_p_multiply(a.item(), b.item())
+        a = randint(MIN, MAX)
+        b = randint(MIN, MAX)
+        assert (gf(a % MOD) * gf(b % MOD)) == ff_p_multiply(a, b)
 
     print(
         f'passed {ROUNDS} randomized {"multiplication":>16} tests !\t{time() - start:>16.2f} s')
 
     start = time()
     for _ in range(ROUNDS):
-        a = gf.Random()
-        b = gf.Random()
-        assert (a ** b.item()) == ff_p_exponentiate(a.item(), b.item())
+        a = randint(MIN, MAX)
+        b = randint(MIN, MAX)
+        assert (gf(a % MOD) ** b) == ff_p_exponentiate(a, b)
 
     print(
         f'passed {ROUNDS} randomized {"exponentiation":>16} tests !\t{time() - start:>16.2f} s')
@@ -50,17 +54,17 @@ def main():
     start = time()
     one = gf(1)
     for _ in range(ROUNDS):
-        a = gf.Random()
-        assert (one / a) == ff_p_inverse(a.item())
+        a = randint(MIN, MAX)
+        assert (one / gf(a % MOD)) == ff_p_inverse(a)
 
     print(
         f'passed {ROUNDS} randomized {"inversion":>16} tests !\t{time() - start:>16.2f} s')
 
     start = time()
     for _ in range(ROUNDS):
-        a = gf.Random()
-        b = gf.Random()
-        assert (a / b) == ff_p_divide(a.item(), b.item())
+        a = randint(MIN, MAX)
+        b = randint(MIN, MAX)
+        assert (gf(a % MOD) / gf(b % MOD)) == ff_p_divide(a, b)
 
     print(
         f'passed {ROUNDS} randomized {"division":>16} tests !\t{time() - start:>16.2f} s')
