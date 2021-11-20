2ae6926533e223226bd6d5fcdb4bd437cd1d5009
Anjan Roy <anjanroy@yandex.com>
2021-11-20 03:20:41 +0000

    added all test cases in test infra runner

--- tests/main.cpp
+++ tests/main.cpp
@@ -21,26 +21,41 @@ int main(int argc, char **argv) {
   std::cout << "✅ passed exponentiation tests" << std::endl;
   test_inversion(q);
   std::cout << "✅ passed inversion tests" << std::endl;
+
   test_alphas(q);
   test_sbox(q);
   test_inv_sbox(q);
   test_permutation(q);
   std::cout << "✅ passed rescue prime tests" << std::endl;
+
   check_ntt_correctness(q, 1 << 10, 1 << 6);
   std::cout << "✅ passed NTT correctness test" << std::endl;
+
   check_ntt_forward_inverse_transform(q, 1 << 10, 1 << 6);
   std::cout << "✅ passed NTT forward/ inverse transform test" << std::endl;
+
   check_cooley_tukey_ntt(q, 1 << 10, 1 << 6);
   std::cout << "✅ passed Cooley-Tukey NTT test" << std::endl;
+
   check_matrix_transposition(q, 1 << 10, 1 << 6);
   std::cout << "✅ passed square matrix transposition test" << std::endl;
+
   test_twiddle_factor_multiplication(q, 1 << 10, 1 << 10, 1 << 6);
   test_twiddle_factor_multiplication(q, 1 << 10, 1 << 11, 1 << 6);
   std::cout << "✅ passed twiddle factor multiplication test [rect/ square]"
             << std::endl;
+
   test_six_step_fft(q, 1 << 20, 1 << 7);
   test_six_step_fft(q, 1 << 21, 1 << 7);
   std::cout << "✅ passed six step FFT test [rect/ square]" << std::endl;
+
+  test_six_step_ifft(q, 1 << 20, 1 << 7);
+  test_six_step_ifft(q, 1 << 21, 1 << 7);
+  std::cout << "✅ passed six step IFFT test [rect/ square]" << std::endl;
+
+  test_six_step_fft_and_ifft(q, 1 << 20, 1 << 8);
+  test_six_step_fft_and_ifft(q, 1 << 21, 1 << 8);
+  std::cout << "✅ passed six step (I)FFT cycle test [rect/ square]" << std::endl;
 
   return 0;
 }
