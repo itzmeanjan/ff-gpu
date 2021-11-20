5a25de7a5a379ec915e96ab28c0ead68ef8df149
Anjan Roy <anjanroy@yandex.com>
2021-11-20 02:33:25 +0000

    six step IFFT based NTT implementation

--- include/ntt.hpp
+++ include/ntt.hpp
@@ -98,5 +98,19 @@ sycl::event twiddle_multiplication(sycl::queue &q, uint64_t *vec,
                                    const uint64_t wg_size,
                                    std::vector<sycl::event> evts);
 
+// Six step FFT algorithm based NTT implementation, which I've 
+// adapted from https://doi.org/10.1109/FPT.2013.6718406
+//
+// @note Returns forward transform result in same input vector
+// so you'll loose your input, if you don't keep another copy of it
 void six_step_fft(sycl::queue &q, uint64_t *vec, const uint64_t dim,
                   const uint64_t wg_size);
+
+// Six step FFT algorithm based Inverse NTT implementation, which I've 
+// adapted from https://doi.org/10.1109/FPT.2013.6718406 and 
+// https://github.com/itzmeanjan/ff-gpu/blob/4d1cedb6ce5144382622632d7809d986543a95a0/reference/ntt.py#L154-L223
+//
+// @note Returns inverse transform result in same input vector
+// so you'll loose your input, if you don't keep another copied version of it
+void six_step_ifft(sycl::queue &q, uint64_t *vec, const uint64_t dim,
+                   const uint64_t wg_size);
