2612857387890e8d634667247e3982f009c5deb9
Anjan Roy <anjanroy@yandex.com>
2021-12-23 13:02:19 +0000

    updated rescue prime benchmark code

--- bench_rescue_prime.cpp
+++ bench_rescue_prime.cpp
@@ -7,22 +7,45 @@ benchmark_hash_elements(sycl::queue& q,
                         const uint32_t wg_size,
                         const uint32_t itr_count)
 {
-  uint64_t* elements =
-    static_cast<uint64_t*>(sycl::malloc_device(sizeof(uint64_t) * 8, q));
-  uint64_t* hashes = static_cast<uint64_t*>(
-    sycl::malloc_device(sizeof(uint64_t) * dim * dim * DIGEST_SIZE, q));
+  sycl::ulong* elements =
+    static_cast<sycl::ulong*>(sycl::malloc_device(sizeof(sycl::ulong) * 8, q));
+  sycl::ulong* hashes = static_cast<sycl::ulong*>(
+    sycl::malloc_device(sizeof(sycl::ulong) * dim * dim * DIGEST_SIZE, q));
+  sycl::ulong16* mds_h = static_cast<sycl::ulong16*>(
+    sycl::malloc_host(sizeof(sycl::ulong16) * STATE_WIDTH, q));
+  sycl::ulong16* ark1_h = static_cast<sycl::ulong16*>(
+    sycl::malloc_host(sizeof(sycl::ulong16) * NUM_ROUNDS, q));
+  sycl::ulong16* ark2_h = static_cast<sycl::ulong16*>(
+    sycl::malloc_host(sizeof(sycl::ulong16) * NUM_ROUNDS, q));
+  sycl::ulong16* mds_d = static_cast<sycl::ulong16*>(
+    sycl::malloc_device(sizeof(sycl::ulong16) * STATE_WIDTH, q));
+  sycl::ulong16* ark1_d = static_cast<sycl::ulong16*>(
+    sycl::malloc_device(sizeof(sycl::ulong16) * NUM_ROUNDS, q));
+  sycl::ulong16* ark2_d = static_cast<sycl::ulong16*>(
+    sycl::malloc_device(sizeof(sycl::ulong16) * NUM_ROUNDS, q));
 
-  auto evt_0 = q.single_task([=]() {
+  prepare_mds(mds_h);
+  prepare_ark1(ark1_h);
+  prepare_ark2(ark2_h);
+
+  sycl::event evt_0 = q.single_task([=]() {
     for (uint8_t i = 0; i < 8; i++) {
       *(elements + i) = i;
     }
   });
 
-  using tp = std::chrono::_V2::steady_clock::time_point;
-  tp start = std::chrono::steady_clock::now();
+  sycl::event evt_1 =
+    q.memcpy(mds_d, mds_h, sizeof(sycl::ulong16) * STATE_WIDTH);
+  sycl::event evt_2 =
+    q.memcpy(ark1_d, ark1_h, sizeof(sycl::ulong16) * NUM_ROUNDS);
+  sycl::event evt_3 =
+    q.memcpy(ark2_d, ark2_h, sizeof(sycl::ulong16) * NUM_ROUNDS);
 
-  auto evt_1 = q.submit([&](sycl::handler& h) {
-    h.depends_on({ evt_0 });
+  using tp = std::chrono::_V2::high_resolution_clock::time_point;
+  tp start = std::chrono::high_resolution_clock::now();
+
+  sycl::event evt_4 = q.submit([&](sycl::handler& h) {
+    h.depends_on({ evt_0, evt_1, evt_2, evt_3 });
     h.parallel_for<class kernelBenchmarkRescuePrimeHash>(
       sycl::nd_range<2>{ sycl::range<2>{ dim, dim },
                          sycl::range<2>{ 1, wg_size } },
@@ -30,16 +53,22 @@ benchmark_hash_elements(sycl::queue& q,
         const size_t idx = it.get_global_linear_id();
 
         for (uint32_t i = 0; i < itr_count; i++) {
-          hash_elements(elements, 8, hashes + idx * 4);
+          hash_elements(elements, 8, hashes + idx * 4, mds_d, ark1_d, ark2_d);
         }
       });
   });
-  evt_1.wait();
+  evt_4.wait();
 
-  tp end = std::chrono::steady_clock::now();
+  tp end = std::chrono::high_resolution_clock::now();
 
   sycl::free(elements, q);
   sycl::free(hashes, q);
+  sycl::free(mds_h, q);
+  sycl::free(ark1_h, q);
+  sycl::free(ark2_h, q);
+  sycl::free(mds_d, q);
+  sycl::free(ark1_d, q);
+  sycl::free(ark2_d, q);
 
   return std::chrono::duration_cast<std::chrono::microseconds>(end - start)
     .count();
