8ffb8f4b9005f2a053fed13049cba2dcc74d3b3b
Anjan Roy <anjanroy@yandex.com>
2022-01-11 14:26:17 +0000

    using class syntax for python wrapper library; setting up sycl queue only once and using for all future invocations

--- wrapper/python/ff_p.py
+++ wrapper/python/ff_p.py
@@ -7,70 +7,94 @@
 '''
 
 import ctypes
-
-ff_p_lib = ctypes.CDLL("../libff_p.so")
-
-
-def ff_p_add(a: int, b: int):
-    '''
-    Modular addition of two prime field elements
-    '''
-    ff_p_lib.add.restype = ctypes.c_uint64
-    ff_p_lib.add.argtypes = [ctypes.c_uint64, ctypes.c_uint64]
-
-    return ff_p_lib.add(a, b)
+from genericpath import exists
+from posixpath import abspath
 
 
-def ff_p_sub(a: int, b: int):
-    '''
-    Modular subtraction of two prime field elements
-    '''
-    ff_p_lib.sub.restype = ctypes.c_uint64
-    ff_p_lib.sub.argtypes = [ctypes.c_uint64, ctypes.c_uint64]
+class ff_p:
+    so_path: str = '../libff_p.so'
+    sycl_q: ctypes.c_void_p = None
+    so_lib: ctypes.CDLL = None
 
-    return ff_p_lib.sub(a, b)
+    def __init__(self) -> None:
+        '''
+        Creates an instance of `ff_p` class, along with backend resource(s)
+        like setting up SYCL queue where compute jobs will be submitted,
+        getting shared library ready for forwarding future function invocations
+        '''
+        if not exists(self.so_path):
+            raise Exception(
+                f'failed to find shared library `{abspath(self.so_path)}`')
 
+        self.so_lib = ctypes.CDLL(self.so_path)
 
-def ff_p_multiply(a: int, b: int):
-    '''
-    Modular multiplication of two prime field elements
-    '''
+        self.so_lib.make_queue.argtypes = [ctypes.POINTER(ctypes.c_void_p)]
+        self.sycl_q = ctypes.c_void_p()
+        self.so_lib.make_queue(ctypes.byref(self.sycl_q))
 
-    ff_p_lib.multiply.restype = ctypes.c_uint64
-    ff_p_lib.multiply.argtypes = [ctypes.c_uint64, ctypes.c_uint64]
+        if self.sycl_q.value == None:
+            raise Exception(f'failed to get default SYCL queue')
 
-    return ff_p_lib.multiply(a, b)
+    def add(self, a: int, b: int) -> int:
+        '''
+        Modular addition of two prime field elements
+        '''
+        self.so_lib.add.restype = ctypes.c_uint64
+        self.so_lib.add.argtypes = [
+            ctypes.c_void_p, ctypes.c_uint64, ctypes.c_uint64]
 
+        return self.so_lib.add(self.sycl_q, a, b)
 
-def ff_p_exponentiate(a: int, b: int):
-    '''
-      Modular exponentiation of one prime field element
-      by exponent (second operand)
-    '''
-    ff_p_lib.exponentiate.restype = ctypes.c_uint64
-    ff_p_lib.exponentiate.argtypes = [ctypes.c_uint64, ctypes.c_uint64]
+    def sub(self, a: int, b: int) -> int:
+        '''
+        Modular subtraction of two prime field elements
+        '''
+        self.so_lib.sub.restype = ctypes.c_uint64
+        self.so_lib.sub.argtypes = [
+            ctypes.c_void_p, ctypes.c_uint64, ctypes.c_uint64]
 
-    return ff_p_lib.exponentiate(a, b)
+        return self.so_lib.sub(self.sycl_q, a, b)
 
+    def multiply(self, a: int, b: int) -> int:
+        '''
+        Modular multiplication of two prime field elements
+        '''
 
-def ff_p_inverse(a: int):
-    '''
-    Muliplicative identity of non-zero prime field element
-    '''
+        self.so_lib.multiply.restype = ctypes.c_uint64
+        self.so_lib.multiply.argtypes = [
+            ctypes.c_void_p, ctypes.c_uint64, ctypes.c_uint64]
 
-    ff_p_lib.inverse.restype = ctypes.c_uint64
-    ff_p_lib.inverse.argtypes = [ctypes.c_uint64]
+        return self.so_lib.multiply(self.sycl_q, a, b)
 
-    return ff_p_lib.inverse(a)
+    def exponentiate(self, a: int, b: int) -> int:
+        '''
+        Modular exponentiation of one prime field element
+        by exponent (second operand)
+        '''
+        self.so_lib.exponentiate.restype = ctypes.c_uint64
+        self.so_lib.exponentiate.argtypes = [
+            ctypes.c_void_p, ctypes.c_uint64, ctypes.c_uint64]
 
+        return self.so_lib.exponentiate(self.sycl_q, a, b)
 
-def ff_p_divide(a: int, b: int):
-    '''
-    Modular division of one prime field element
-    by another one
-    '''
+    def inverse(self, a: int) -> int:
+        '''
+        Muliplicative identity of non-zero prime field element
+        '''
 
-    ff_p_lib.divide.restype = ctypes.c_uint64
-    ff_p_lib.divide.argtypes = [ctypes.c_uint64, ctypes.c_uint64]
+        self.so_lib.inverse.restype = ctypes.c_uint64
+        self.so_lib.inverse.argtypes = [ctypes.c_void_p, ctypes.c_uint64]
 
-    return ff_p_lib.divide(a, b)
+        return self.so_lib.inverse(self.sycl_q, a)
+
+    def divide(self, a: int, b: int) -> int:
+        '''
+        Modular division of one prime field element
+        by another one
+        '''
+
+        self.so_lib.divide.restype = ctypes.c_uint64
+        self.so_lib.divide.argtypes = [
+            ctypes.c_void_p, ctypes.c_uint64, ctypes.c_uint64]
+
+        return self.so_lib.divide(self.sycl_q, a, b)
